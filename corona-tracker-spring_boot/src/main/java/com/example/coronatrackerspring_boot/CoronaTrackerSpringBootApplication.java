410c1990afdb22b3d0e66e8f40b809a7b6b6dcdd
isakulaksiz <isakulaksiz@outlook.com>
2020-09-30 17:17:03 +0300

    Transform CSV(limited) file

--- corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/CoronaTrackerSpringBootApplication.java
+++ corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/CoronaTrackerSpringBootApplication.java
@@ -2,9 +2,10 @@ package com.example.coronatrackerspring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-
+@EnableScheduling
 public class CoronaTrackerSpringBootApplication {
 
 	public static void main(String[] args) {
--- corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/services/CoronaVirusDataService.java
+++ corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/services/CoronaVirusDataService.java
@@ -1,9 +1,13 @@
 package com.example.coronatrackerspring_boot.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,15 +18,19 @@ public class CoronaVirusDataService {
     private static String URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
-
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
-
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
 
 }
