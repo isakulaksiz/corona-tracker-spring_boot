c3fe795ddb8fbd10a9cb9f3323f53e1973254967
isakulaksiz <isakulaksiz@outlook.com>
2020-09-30 17:13:30 +0300

    Add api-service

---
+++ corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/services/CoronaVirusDataService.java
@@ -0,0 +1,28 @@
+package com.example.coronatrackerspring_boot.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class CoronaVirusDataService {
+    private static String URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    @PostConstruct
+
+    public void fetchData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+
+    }
+
+}
