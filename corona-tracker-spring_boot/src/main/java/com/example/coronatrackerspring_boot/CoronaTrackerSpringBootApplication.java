7c1c52c563664466e6e9a4028c4830f9a9aee2d7
isakulaksiz <isakulaksiz@outlook.com>
2020-09-30 17:00:43 +0300

    Initial commit

---
+++ corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/CoronaTrackerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.coronatrackerspring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerSpringBootApplication.class, args);
+	}
+
+}
