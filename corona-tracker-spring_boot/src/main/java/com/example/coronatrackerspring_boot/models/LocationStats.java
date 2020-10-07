7e05f1e4dcd584c8874cd3113706546c53f311c1
isakulaksiz <isakulaksiz@outlook.com>
2020-10-07 12:29:04 +0300

    feat: Module added

---
+++ corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/models/LocationStats.java
@@ -0,0 +1,41 @@
+package com.example.coronatrackerspring_boot.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int totalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getTotalCases() {
+        return totalCases;
+    }
+
+    public void setTotalCases(int totalCases) {
+        this.totalCases = totalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", totalCases=" + totalCases +
+                '}';
+    }
+}
--- corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/services/CoronaVirusDataService.java
+++ corona-tracker-spring_boot/src/main/java/com/example/coronatrackerspring_boot/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package com.example.coronatrackerspring_boot.services;
 
+import com.example.coronatrackerspring_boot.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,14 +13,17 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-public class CoronaVirusDataService {
+public class    CoronaVirusDataService {
     private static String URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
+    private List<LocationStats> allStats = new ArrayList<>();
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(URL))
@@ -28,9 +32,13 @@ public class CoronaVirusDataService {
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStats = new LocationStats();
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setCountry(record.get("Country/Region"));
+            locationStats.setTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStats);
+            newStats.add(locationStats);
         }
+        this.allStats = newStats;
     }
-
 }
